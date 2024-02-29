6da00549e92a838f490e1a5923ea721e6236bf13
Name <email@exaple.ru>
2024-02-29 10:48:11 +0300

    Добавьте файлы проекта.

---
+++ LifeSpot/Static/JS/about.js
@@ -0,0 +1,50 @@
+﻿/*
+* Запросим пользовательский ввод
+* и сохраним отзыв в объект
+* 
+* */
+function getComment() {
+    // Создадим объект
+    function Review() {
+        this.userName = prompt("Как вас зовут ?"),
+            this.comment = prompt("Напишите свой отзыв"),
+            this.date = new Date().toLocaleString()
+    };
+
+    let feedback = new Review();
+
+    if (confirm('Хотите чтобы ваш отзыв могли оценить другие пользователи?')) {
+        feedback.rate = 0;
+    }
+    writeReview(feedback);
+}
+
+/*
+* Запишем отзыв на страницу 
+* 
+* */
+const writeReview = review => {
+    let rateComment = '';
+    if (review.hasOwnProperty('rate')) {
+        let buttonId = Math.random();
+        rateComment += '<b style="color: green">Рейтинг: </b>' + '<button style=" border: none" backgroundcolor:  " id="' + buttonId + '" onclick="addLike(this.id)">' + review.rate + ' ❤️' + '</button>';
+    }
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']} ${rateComment}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
+}
+
+function addLike(id) {
+    let buttonLike = document.getElementById(id);
+
+    let array = buttonLike.innerText.split(' ');
+
+    let numLike = parseInt(array[0], 10);
+
+    numLike++;
+
+    array[0] = numLike;
+
+    buttonLike.innerText = array.join(' ');
+}
---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,88 @@
+﻿/*
+* Сессия теперь создается в общей области видимости.
+* Будет "захватываться" тремя функциями
+* 
+* */
+
+/*
+* Сохранение данных сессии сразу при заходе пользователя на страницу
+* 
+* */
+
+function handleSession(sessionLoger, checkUserAge) {
+    if (sessionStorage.getItem("startDate") == null) {
+        sessionStorage.setItem("startDate", new Date().toLocaleString());
+    }
+    if (sessionStorage.getItem("startAgent") == null) {
+        sessionStorage.setItem("userAgent", window.navigator.userAgent);
+    }
+    if (sessionStorage.getItem("userAge") == null) {
+        sessionStorage.setItem("userAge", prompt("Пожалуйста, введите ваш возраст?"));
+        checkUserAge(true);
+    }
+    else {
+        checkUserAge(false);
+    }
+    sessionLoger();
+}
+
+///*
+//* Проверка возраста пользователя
+//* 
+//* */
+function checkAge(newVisit) {
+
+    if (sessionStorage.getItem("userAge") >= 18) {
+        if (newVisit)
+        {
+            alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+        }
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+
+///*
+//* Вывод данных сессии в консоль
+//* 
+//* */
+let sessionLog = function () {
+    console.log(sessionStorage.getItem("startDate"));
+    console.log(sessionStorage.getItem("userAge"));
+    console.log(sessionStorage.getItem("userAgent"));
+}
+
+///*
+//* Функция для фильтраци контента
+//* Будет вызываться благодаря атрибуту oninput на index.html
+//* 
+//* */
+
+function filterContent() {
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++) {
+        let videoText = elements[i].getElementsByTagName('h3')[0].innerText;
+
+        if (!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())) {
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+
+///*
+//* Всплывающее окно будет показано по таймауту
+//* 
+//* */
+//// setTimeout(() =>
+////     alert("Нравится LifeSpot? " + '\n' +  "Подпишитесь на наш Instagram @lifespot999!" ),
+//// 7000);
+
+
+
+
